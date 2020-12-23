45028d401d29f145b513dd0172112c3bddbcffc0
KonungGudleifr <72839083+KonungGudleifr@users.noreply.github.com>
2020-12-23 22:47:13 +0200

    Add files via upload

---
+++ 3.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChernobylApp3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double result = 0;
+                Console.WriteLine("Введите число рядов: ");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите число x: ");
+                int x = int.Parse(Console.ReadLine());
+                for (int i = 0; i < n; i++)
+                {
+                    result += (Math.Cos(i * x) / 3);
+                }
+                Console.WriteLine($"Сумма {n} рядов = {result}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
